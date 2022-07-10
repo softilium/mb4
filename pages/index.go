28f71749e6932123371b40f1d226c84b3535932b
Nick Mokhnatov <softilium@gmail.com>
2022-07-10 23:00:16 +0300

    mark buy candidates on homepage (top dsi + top div yield + fallen)

--- pages/index.go
+++ pages/index.go
@@ -15,13 +15,16 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	topYield5Y := cube.Market.TopDivYields5Y(20)
-	topDSI := cube.Market.TopDSI(20)
-	topFallen := cube.Market.TopFallenRaise(20, false)
-	topRaise := cube.Market.TopFallenRaise(20, true)
+	topxx := 25
+
+	topYield5Y := cube.Market.TopDivYields5Y(topxx)
+	topDSI := cube.Market.TopDSI(topxx)
+	topFallen := cube.Market.TopFallenRaise(topxx, false)
+	topRaise := cube.Market.TopFallenRaise(topxx, true)
 
 	type TopItem struct {
 		Ticker string
+		Star   bool
 		Descr  string
 		V      string
 		V1     string
@@ -61,6 +64,35 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		pd.TopRaise[k].Ticker = "/ticker?id=" + v.Cell.Quote.Edges.Ticker.ID
 		pd.TopRaise[k].Descr = v.Cell.Quote.Edges.Ticker.Descr
 		pd.TopRaise[k].V = fmt.Sprintf("%.1f", v.PercentPriceChange)
+	}
+
+	// count stars
+	starmap := make(map[string]int)
+	for _, ti := range pd.TopY {
+		starmap[ti.Ticker] = starmap[ti.Ticker] + 1
+	}
+	for _, ti := range pd.TopDSI {
+		starmap[ti.Ticker] = starmap[ti.Ticker] + 1
+	}
+	for _, ti := range pd.TopFallen {
+		starmap[ti.Ticker] = starmap[ti.Ticker] + 1
+	}
+
+	// set stars
+	for i, ti := range pd.TopY {
+		if starmap[ti.Ticker] > 2 {
+			pd.TopY[i].Star = true
+		}
+	}
+	for i, ti := range pd.TopDSI {
+		if starmap[ti.Ticker] > 2 {
+			pd.TopDSI[i].Star = true
+		}
+	}
+	for i, ti := range pd.TopFallen {
+		if starmap[ti.Ticker] > 2 {
+			pd.TopFallen[i].Star = true
+		}
 	}
 
 	tmpl, err := pongo2.FromCache("pages/index.html")
