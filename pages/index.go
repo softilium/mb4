9ea7b94c4247ec0c6000ed71a13e0871aff461c6
Nick Mokhnatov <softilium@gmail.com>
2022-07-10 23:22:30 +0300

    optomize homepage handler

--- pages/index.go
+++ pages/index.go
@@ -32,40 +32,47 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 	type pageDataStruct struct {
 		SessionStruct
-		TopY      []TopItem
-		TopDSI    []TopItem
-		TopFallen []TopItem
-		TopRaise  []TopItem
+		TopY      []*TopItem
+		TopDSI    []*TopItem
+		TopFallen []*TopItem
+		TopRaise  []*TopItem
 	}
 
 	pd := pageDataStruct{SessionStruct: LoadSessionStruct(r)}
-	pd.TopY = make([]TopItem, len(topYield5Y))
+	pd.TopY = make([]*TopItem, len(topYield5Y))
 	for k, v := range topYield5Y {
-		pd.TopY[k].Ticker = "/ticker?id=" + v.Quote.Edges.Ticker.ID
-		pd.TopY[k].Descr = v.Quote.Edges.Ticker.Descr
-		pd.TopY[k].V = fmt.Sprintf("%.1f", v.DivYield5Y)
-		pd.TopY[k].V1 = fmt.Sprintf("%.1f", v.DSI)
+		pd.TopY[k] = &TopItem{
+			Ticker: "/ticker?id=" + v.Quote.Edges.Ticker.ID,
+			Descr:  v.Quote.Edges.Ticker.Descr,
+			V:      fmt.Sprintf("%.1f", v.DivYield5Y),
+			V1:     fmt.Sprintf("%.1f", v.DSI),
+		}
 	}
-	pd.TopDSI = make([]TopItem, len(topDSI))
+	pd.TopDSI = make([]*TopItem, len(topDSI))
 	for k, v := range topDSI {
-		pd.TopDSI[k].Ticker = "/ticker?id=" + v.Quote.Edges.Ticker.ID
-		pd.TopDSI[k].Descr = v.Quote.Edges.Ticker.Descr
-		pd.TopDSI[k].V = fmt.Sprintf("%.1f", v.DivYield5Y)
-		pd.TopDSI[k].V1 = fmt.Sprintf("%.1f", v.DSI)
+		pd.TopDSI[k] = &TopItem{
+			Ticker: "/ticker?id=" + v.Quote.Edges.Ticker.ID,
+			Descr:  v.Quote.Edges.Ticker.Descr,
+			V:      fmt.Sprintf("%.1f", v.DivYield5Y),
+			V1:     fmt.Sprintf("%.1f", v.DSI),
+		}
 	}
-	pd.TopFallen = make([]TopItem, len(topFallen))
+	pd.TopFallen = make([]*TopItem, len(topFallen))
 	for k, v := range topFallen {
-		pd.TopFallen[k].Ticker = "/ticker?id=" + v.Cell.Quote.Edges.Ticker.ID
-		pd.TopFallen[k].Descr = v.Cell.Quote.Edges.Ticker.Descr
-		pd.TopFallen[k].V = fmt.Sprintf("%.1f", -v.PercentPriceChange)
+		pd.TopFallen[k] = &TopItem{
+			Ticker: "/ticker?id=" + v.Cell.Quote.Edges.Ticker.ID,
+			Descr:  v.Cell.Quote.Edges.Ticker.Descr,
+			V:      fmt.Sprintf("%.1f", -v.PercentPriceChange),
+		}
 	}
-	pd.TopRaise = make([]TopItem, len(topRaise))
+	pd.TopRaise = make([]*TopItem, len(topRaise))
 	for k, v := range topRaise {
-		pd.TopRaise[k].Ticker = "/ticker?id=" + v.Cell.Quote.Edges.Ticker.ID
-		pd.TopRaise[k].Descr = v.Cell.Quote.Edges.Ticker.Descr
-		pd.TopRaise[k].V = fmt.Sprintf("%.1f", v.PercentPriceChange)
+		pd.TopRaise[k] = &TopItem{
+			Ticker: "/ticker?id=" + v.Cell.Quote.Edges.Ticker.ID,
+			Descr:  v.Cell.Quote.Edges.Ticker.Descr,
+			V:      fmt.Sprintf("%.1f", v.PercentPriceChange),
+		}
 	}
-
 	// count stars
 	starmap := make(map[string]int)
 	for _, ti := range pd.TopY {
@@ -79,19 +86,19 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// set stars
-	for i, ti := range pd.TopY {
+	for _, ti := range pd.TopY {
 		if starmap[ti.Ticker] > 2 {
-			pd.TopY[i].Star = true
+			ti.Star = true
 		}
 	}
-	for i, ti := range pd.TopDSI {
+	for _, ti := range pd.TopDSI {
 		if starmap[ti.Ticker] > 2 {
-			pd.TopDSI[i].Star = true
+			ti.Star = true
 		}
 	}
-	for i, ti := range pd.TopFallen {
+	for _, ti := range pd.TopFallen {
 		if starmap[ti.Ticker] > 2 {
-			pd.TopFallen[i].Star = true
+			ti.Star = true
 		}
 	}
 
