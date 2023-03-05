cc69baf6547ce7c871af74fe16f612c5babceb03
Nick Mokhnatov <softilium@gmail.com>
2023-03-05 21:18:40 +0100

    del

--- deliver.sh
+++
@@ -1,8 +0,0 @@
-rm mb4.rar
-rar a mb4.rar mb4 pages/*.html assets/* -r -s
-sshpass -p 'vMbXcWBTensq' scp mb4.rar ubuntu@162.19.152.177:/var/www/mb4/
-
-sshpass -p 'vMbXcWBTensq' ssh ubuntu@162.19.152.177 "sudo systemctl stop mb4.service"
-sshpass -p 'vMbXcWBTensq' ssh ubuntu@162.19.152.177 "cd /var/www/mb4/ ; unrar x -y mb4.rar"
-sshpass -p 'vMbXcWBTensq' ssh ubuntu@162.19.152.177 "cd /var/www/mb4/ ; chmod +x mb4"
-sshpass -p 'vMbXcWBTensq' ssh ubuntu@162.19.152.177 "sudo systemctl start mb4.service"
